62daf4a358f03d05b2f06ff00de2a91c0625e984
Sam Davenport <samueldavenport@att.net>
2021-10-19 17:01:34 -0500

    textarea data now persists in localStorage. CSS classes for past, present, future now apply according to the current time of day.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
-var nine = moment().hours(9).format("h a");
-var ten = moment().hours(10).format("h a");
-var elev = moment().hours(11).format("h a");
-var twelve = moment().hours(12).format("h a");
-var one = moment().hours(13).format("h a");
-var two = moment().hours(14).format("h a");
-var three = moment().hours(15).format("h a");
-var four = moment().hours(16).format("h a");
-var five = moment().hours(17).format("h a");
+// var nine = moment().hours(9);
+// var ten = moment().hours(10);
+// var elev = moment().hours(11);
+// var twelve = moment().hours(12);
+// var one = moment().hours(13);
+// var two = moment().hours(14);
+// var three = moment().hours(15);
+// var four = moment().hours(16);
+// var five = moment().hours(17);
+
+
+
+
+
 
 var timesArr = [
-     nine,
-     ten,
-     elev,
-     twelve,
-     one,
-     two,
-     three,
-     four,
-     five
+     9,
+     10,
+     11,
+     12,
+     13,
+     14,
+     15,
+     16,
+     17
 ]
-
+console.log(timesArr);
 
 window.onload = displayDate(); loadSchedule();
 // makes date/time current up to the second
@@ -28,24 +33,50 @@ function displayDate(){
   document.getElementById('currentDay').innerHTML = day;
   setTimeout(displayDate, 1000); 
 }
- // makes set time load into each time block
- //function loadTimes(){
-  //   document.querySelector('.hour').innerHTML = nine;
- //}
 
- // appends rows to page
- function loadSchedule(){
-     
-    timesArr.forEach(time => {
-        var timeBlock = $("<div></div>").addClass('row time-block');
-        var hourBlock = $("<div>" + time + "</div>").addClass('hour col-2 col-lg-1');
-        var textBlock = $("<textarea></textarea>").addClass('col-7 col-lg-9 d-flex');
-        var inputBlock = $("<input />", { type: 'checkbox'}).addClass('check col-1 col-lg-1');
-        var button = $("<button>" + "Save" + "</button>").addClass('saveBtn col-2 col-md-1 col-lg-1');
-        timeBlock.append(hourBlock);
-        timeBlock.append(textBlock);
-        timeBlock.append(inputBlock);
-        timeBlock.append(button);
-        $(".container").append(timeBlock);
-    });
-};
+    
+// makes set time load into each time block
+    function loadSchedule(){
+        
+        timesArr.forEach(time => {
+           //var timeKey = time.split(" ")[0]
+            var timeBlock = $("<div></div>").addClass('row time-block').attr("id", time);
+            var hourBlock = $("<div>" + moment().hours(time).format('h a') + "</div>").addClass('hour col-2 col-lg-1');
+            var textBlock = $("<textarea></textarea>").addClass('col-7 col-lg-9 d-flex plan-text');
+            var inputBlock = $("<input />", { type: 'checkbox'}).addClass('check col-1 col-lg-1');
+            var button = $("<button>" + "Save" + "</button>").addClass('saveBtn col-2 col-md-1 col-lg-1');
+
+            if (time < moment().hours()) {
+                textBlock.addClass('past')
+            }else if (time === moment().hours()) {
+                textBlock.removeClass('past');
+                textBlock.addClass('present');
+            }else {
+                textBlock.removeClass('past');
+                textBlock.removeClass('present');
+                textBlock.addClass('future');
+            }
+
+
+            textBlock.val(localStorage.getItem(time))
+            timeBlock.append(hourBlock);
+            timeBlock.append(textBlock);
+            timeBlock.append(inputBlock);
+            timeBlock.append(button);
+            // appends rows to page
+            $(".container").append(timeBlock);
+            
+        });
+    };
+    
+    $(".saveBtn").on('click', function () {
+        var activity = $(this).siblings(".plan-text").val()
+        var hour = $(this).siblings(".hour").text()
+        var hourKey = hour.split(" ")[0]
+        localStorage.setItem(hourKey, activity)
+      //  textBlock.innerHTML = planText;
+        // set an array of planText data to localStorage
+      //  localStorage.setItem("planTextArr", planText );
+       // console.log(planText);
+       console.log(hour);
+    }) 
