499901d05f99c94aa4d5108ab67619c021619d0f
Sam Davenport <samueldavenport@att.net>
2021-10-19 21:43:27 -0500

    bug fixed with persistence in localStorage. save button icon in place. checkbox now strikes through plan text.

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,11 +40,19 @@ function displayDate(){
         
         timesArr.forEach(time => {
            //var timeKey = time.split(" ")[0]
+           
             var timeBlock = $("<div></div>").addClass('row time-block').attr("id", time);
             var hourBlock = $("<div>" + moment().hours(time).format('h a') + "</div>").addClass('hour col-2 col-lg-1');
             var textBlock = $("<textarea></textarea>").addClass('col-7 col-lg-9 d-flex plan-text');
-            var inputBlock = $("<input />", { type: 'checkbox'}).addClass('check col-1 col-lg-1');
-            var button = $("<button>" + "Save" + "</button>").addClass('saveBtn col-2 col-md-1 col-lg-1');
+            var inputBlock = $("<input />", { type: 'checkbox'}).addClass('check col-1 col-lg-1 checkbox');
+            var button = $("<button></button>").addClass('saveBtn col-2 col-md-1 col-lg-1 fas fa-save fa-5x');
+
+            $('.container').on('change', '.checkbox', function() {
+                var strike = $(this).siblings('.plan-text').toggleClass('line');
+                
+                    
+           
+            });
 
             if (time < moment().hours()) {
                 textBlock.addClass('past')
@@ -58,7 +66,11 @@ function displayDate(){
             }
 
 
-            textBlock.val(localStorage.getItem(time))
+            textBlock.val(localStorage.getItem(time));
+
+           
+
+
             timeBlock.append(hourBlock);
             timeBlock.append(textBlock);
             timeBlock.append(inputBlock);
@@ -67,16 +79,25 @@ function displayDate(){
             $(".container").append(timeBlock);
             
         });
+
     };
-    
     $(".saveBtn").on('click', function () {
         var activity = $(this).siblings(".plan-text").val()
         var hour = $(this).siblings(".hour").text()
         var hourKey = hour.split(" ")[0]
-        localStorage.setItem(hourKey, activity)
+        var hourParse = parseInt(hourKey);
+         if(hourParse < 9){
+             hourParse = hourParse + 12
+         }
+        localStorage.setItem(hourParse , activity)
       //  textBlock.innerHTML = planText;
         // set an array of planText data to localStorage
       //  localStorage.setItem("planTextArr", planText );
        // console.log(planText);
-       console.log(hour);
+       console.log(hourParse);
     }) 
+
+    
+         // strike.attr('style', 'text-decoration: line-through')
+        
+   
